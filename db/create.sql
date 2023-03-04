310f4382bfeb63552916eb4e294fefdab10c372f
Hayden Lauritzen <haydenlauritzen@gmail.com>
2023-03-04 00:52:13 -0800

    update create

--- db/create.sql
+++ db/create.sql
@@ -1,12 +1,26 @@
+-- build test database
 CREATE DATABASE airporttest;
 
 \c airporttest; 
 
 CREATE TABLE test
 (
-    id      INTEGER PRIMARY KEY     NOT NULL,
-    name    VARCHAR(30)             NOT NULL 
+    id      SERIAL          NOT NULL,
+    name    VARCHAR(30)     NOT NULL 
+    PRIMARY KEY(id)
 )   
 ;
 
-CREATE USER test WITH LOGIN AND PASSWORD 'testpassword';
+-- permissions
+CREATE USER test WITH LOGIN PASSWORD 'testpassword';
+GRANT ALL ON DATABASE airporttest TO test;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO test;
+
+-- inserts
+COPY test FROM stdin;
+1 test1
+2 test2
+3 test3
+4 test4
+5 test5
+\.
