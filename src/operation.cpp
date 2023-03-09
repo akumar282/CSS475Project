0d992d17e87cea3870cf685c535922853345a87b
Hayden Lauritzen <haydenlauritzen@gmail.com>
2023-03-08 23:58:35 -0800

    add some comments in operation.h

--- src/operation.cpp
+++ src/operation.cpp
@@ -1,12 +1,16 @@
 #include "../inc/operation.h"
 
 // command mappings
+
+// maps keyword to invoke command to its corresponding id
 const std::map<std::string, operation_t> Operation::commandList = {
     {"exit", Operation::c_exit},
     {"e", Operation::c_exit},
     {"help", Operation::c_help},
     {"h", Operation::c_help}
 };
+
+//maps keyword to its corresponding help message
 const std::map<std::string, std::string> Operation::commandHelp = {
     {"exit", "exit - exits program"},
     {"help", "help - lists all commands"}
